c89da5bf5a9d1125e6d6a3aaa47914eee94033b5
woodminus <160528767+woodminus@users.noreply.github.com>
2020-05-28 19:03:50 +0000

    button fixes

--- Quadrocopter-Box2D/Quadrocopter-Box2D/Quadrocopter-Box2D/Obstacle2D.cpp
+++ Quadrocopter-Box2D/Quadrocopter-Box2D/Quadrocopter-Box2D/Obstacle2D.cpp
@@ -51,4 +51,53 @@ void Obstacle2D::reset () {
 	body->SetTransform (b2Vec2(x, y), angle);
 	
 
-    b2Fixture *f = body->GetFixtureList
+    b2Fixture *f = body->GetFixtureList();
+    body->DestroyFixture(f);
+	
+	b2PolygonShape dynamicBox;
+	dynamicBox.SetAsBox(a * 0.5, b * 0.5);
+	
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &dynamicBox;
+	//filtering collisions between quadrocopters
+	fixtureDef.filter.categoryBits = 0x0004;
+	fixtureDef.filter.maskBits = 0x0002;
+	body->CreateFixture(&fixtureDef);
+
+}
+
+void Obstacle2D::getCoords (
+	float& centerX,
+	float& centerY,
+	float& a,
+	float& b,
+	float& angle
+) const {
+	centerX = body->GetPosition().x;
+	centerY = body->GetPosition().y;
+	a = this->a;
+	b = this->b;
+	angle = radToDeg(body->GetAngle());
+}
+
+void Obstacle2D::getPoints (
+	float& x0,
+	float& y0,
+	float& x1,
+	float& y1,
+	float& x2,
+	float& y2,
+	float& x3,
+	float& y3
+) const {
+
+	float a2, b2, px0, py0, px1, py1, px2, py2, px3, py3;
+	float angle = body->GetAngle() + M_PI_2;
+	
+	float x = body->GetPosition().x;
+	float y = body->GetPosition().y;
+	
+	a2 = a * 0.5;
+	b2 = b * 0.5;
+
+	px0 = - a2; py0 = + b
