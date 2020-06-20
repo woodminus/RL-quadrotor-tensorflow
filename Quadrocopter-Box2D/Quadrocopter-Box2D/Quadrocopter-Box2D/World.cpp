8a82c4f062489654cdf147a63cdcff5e61b1b572
woodminus <160528767+woodminus@users.noreply.github.com>
2020-06-20 19:09:20 +0000

    fix..

--- Quadrocopter-Box2D/Quadrocopter-Box2D/Quadrocopter-Box2D/World.cpp
+++ Quadrocopter-Box2D/Quadrocopter-Box2D/Quadrocopter-Box2D/World.cpp
@@ -2,4 +2,17 @@
 //  World.cpp
 //  Quadrocopter-Box2D
 //
-//  Created by anton on 27/04/
+//  Created by anton on 27/04/16.
+//  Copyright © 2016 anton. All rights reserved.
+//
+
+#include "World.hpp"
+#include "WorldObjectInfo.hpp"
+
+float degToRad (float deg) {
+	return M_PI * deg / 180;
+}
+
+float radToDeg (float rad) {
+	return 180 * rad / M_PI;
+}
---
+++ Quadrocopter-Box2D/Quadrocopter-Box2D/Quadrocopter-Box2D/WorldBase.hpp
@@ -0,0 +1,21 @@
+//
+//  WorldBase.hpp
+//  Quadrocopter-Box2D
+//
+//  Created by anton on 24/06/16.
+//  Copyright © 2016 anton. All rights reserved.
+//
+
+#ifndef WorldBase_hpp
+#define WorldBase_hpp
+
+#include <vector>
+#include <functional>
+#include <iostream>
+
+template <typename QuadrocopterType, typename ObstacleType>
+class WorldBase {
+
+public:
+
+	virtua
