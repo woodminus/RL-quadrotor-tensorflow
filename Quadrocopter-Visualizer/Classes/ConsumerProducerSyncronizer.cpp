98ad8c6f970e770b0e7d3d0be58f37dd9c500b99
woodminus <160528767+woodminus@users.noreply.github.com>
2020-06-30 14:45:57 +0000

    Add error checking

---
+++ Quadrocopter-Visualizer/Classes/ConsumerProducerSyncronizer.cpp
@@ -0,0 +1,11 @@
+//
+//  ConsumerProducerSyncronizer.cpp
+//  Quadrocopter-Visualizer
+//
+//  Created by anton on 29/05/16.
+//
+//
+
+#include "ConsumerProducerSyncronizer.hpp"
+
+ConsumerProducerSyncronizer::Con
