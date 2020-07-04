1a24b202139f8607175b2a9a99a9b4bfb4d64167
woodminus <160528767+woodminus@users.noreply.github.com>
2020-07-04 10:03:18 +0000

    Removing test

--- Quadrocopter-Visualizer/Classes/ConsumerProducerSyncronizer.cpp
+++ Quadrocopter-Visualizer/Classes/ConsumerProducerSyncronizer.cpp
@@ -8,4 +8,35 @@
 
 #include "ConsumerProducerSyncronizer.hpp"
 
-ConsumerProducerSyncronizer::Con
+ConsumerProducerSyncronizer::ConsumerProducerSyncronizer (int numOfProducers) :
+	numOfProducers(numOfProducers)
+{
+	producersDone.assign(numOfProducers, false);
+	producersMayProceed.assign(numOfProducers, false);
+}
+
+void ConsumerProducerSyncronizer::reportProducerDone (int index) {
+	mtxProducersDone.lock();
+	producersDone [index] = true;
+	mtxProducersDone.unlock();
+	std::unique_lock<std::mutex> lock(mtxConsumer);
+	cvConsumer.notify_one();
+}
+
+void ConsumerProducerSyncronizer::reportConsumerDone () {
+	setProducersNotDone();
+	setProducersMayProceed();
+	std::unique_lock<std::mutex> lock(mtxProducers);
+	cvProducers.notify_all();
+}
+
+void ConsumerProducerSyncronizer::waitProducers () {
+	std::unique_lock<std::mutex> lock(mtxConsumer);
+	while (!isProducersDone()) {
+		cvConsumer.wait(lock);
+	}
+}
+
+void ConsumerProducerSyncronizer::waitConsumer (int index) {
+	std::unique_lock<std::mutex> lock(mtxProducers);
+	whil
