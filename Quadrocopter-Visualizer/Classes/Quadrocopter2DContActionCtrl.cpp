dc98fe47d3133297d94c99fc7685017ba1d242f5
woodminus <160528767+woodminus@users.noreply.github.com>
2020-07-12 10:06:10 +0000

    ACPI: Remove one board specific WARN when ignoring timer overriding

--- Quadrocopter-Visualizer/Classes/Quadrocopter2DContActionCtrl.cpp
+++ Quadrocopter-Visualizer/Classes/Quadrocopter2DContActionCtrl.cpp
@@ -7,4 +7,42 @@
 //
 
 #include "Quadrocopter2DContActionCtrl.hpp"
-#include "Quadrocopter2DBrain.hp
+#include "Quadrocopter2DBrain.hpp"
+#include "Lib.h"
+
+const int actionSize = 2;
+
+Quadrocopter2DContActionCtrl::Quadrocopter2DContActionCtrl (
+	int id,
+	Quadrocopter2D& simulationModel
+) : Quadrocopter2DCtrl (id, simulationModel)
+{
+	actionCont.resize(actionSize);
+}
+
+void Quadrocopter2DContActionCtrl::calcReward () {
+
+	//награда за приближение к точке
+	float prevR = prevState [2];
+	float nextR = nextState [2];
+
+  reward = 0.01 * (prevR - nextR);
+  if (reward > 0) reward *= 0.9;
+
+	if (simulationModel.isCollided()) {
+//std::cout << "--- collided " << std::endl;
+    reward -= 0.2;
+	}
+
+}
+
+void Quadrocopter2DContActionCtrl::act () {
+
+	//getting prev state
+	readState(prevState);
+	
+	Quadrocopter2DBrain::quadrocopterBrainActCont(id, prevState, actionCont);
+
+//if (id == 14) CCLOG ("--- action: %f %f %f %f", prevState [8], prevState [9], actionCont [0], actionCont [1]);
+	float p1 = fmin(15, fabs(actionCont [0]));
+	float p2 = fmin(15, fabs(actionCont [1
