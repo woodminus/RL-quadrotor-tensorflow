3d1471b31818a4a6c98a649248e6383ce1e5c95b
woodminus <160528767+woodminus@users.noreply.github.com>
2020-07-26 09:19:33 +0000

    chore: updated .travis.yml

--- Quadrocopter-Visualizer/Classes/QuadrocopterContActionCtrl.cpp
+++ Quadrocopter-Visualizer/Classes/QuadrocopterContActionCtrl.cpp
@@ -31,4 +31,36 @@ void QuadrocopterContActionCtrl::calcReward () {
 
 void QuadrocopterContActionCtrl::act () {
 
-	/
+	//getting prev state
+	readState(prevState);
+	
+	Quadrocopter2DBrain::quadrocopterBrainActCont(id, prevState, actionCont);
+//	actionCont [0] *= 100;
+//if (id == 14) CCLOG ("--- action: %f %f", prevState [0], actionCont [0]);
+	simulationModel.setMotorPower(actionCont [0]);
+
+	reseted = false;
+}
+
+void QuadrocopterContActionCtrl::storeExperience () {
+
+	if (reseted) {
+		return;
+	}
+	
+	readState(nextState);
+	
+	calcReward();
+	
+	Quadrocopter2DBrain::storeQuadrocopterExperienceCont(id, reward, actionCont, prevState, nextState);
+}
+
+void QuadrocopterContActionCtrl::onSimulationStep (int stepIndex) {
+
+	float x = simulationModel.getPosition();
+	if (fabsf(x) > 200) {
+		reset();
+		return;
+	}
+	
+}
---
+++ Quadrocopter-Visualizer/Classes/QuadrocopterContActionCtrl.hpp
@@ -0,0 +1,10 @@
+//
+//  QuadrocopterContActionCtrl.hpp
+//  Quadrocopter-Visualizer
+//
+//  Created by anton on 13/08/16.
+//
+//
+
+#ifndef QuadrocopterContActionCtrl_hpp
+#define QuadrocopterContActionCtrl_
