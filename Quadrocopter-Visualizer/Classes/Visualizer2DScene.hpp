3788661d126cbeca9cf87eab8ee56fe893d3f90a
woodminus <160528767+woodminus@users.noreply.github.com>
2020-08-01 05:52:34 +0000

    f2fs: speed up handling holes in fiemap

--- Quadrocopter-Visualizer/Classes/Visualizer2DScene.hpp
+++ Quadrocopter-Visualizer/Classes/Visualizer2DScene.hpp
@@ -18,4 +18,43 @@ public:
 	
 	void init ();
 	void addTo (cocos2d::Node* parent);
-	void setCoordsFrom (con
+	void setCoordsFrom (const QuadrocopterModel2DIFace& model, cocos2d::Vec2 centerPos, float visualizerZoom);
+
+private:
+
+	float zoom = 50;
+	cocos2d::LayerColor* body;
+	cocos2d::LayerColor* motor1;
+	cocos2d::LayerColor* motor2;
+	cocos2d::DrawNode* sensors;
+	
+	cocos2d::Vec2 bodyHalfCont;
+	cocos2d::Vec2 motor1HalfCont;
+	cocos2d::Vec2 motor2HalfCont;
+};
+
+
+class Obstacle2DView {
+public:
+	
+	void init ();
+	void addTo (cocos2d::Node* parent);
+	void setCoordsFrom (const ObstacleModel2DIFace& model, cocos2d::Vec2 centerPos, float visualizerZoom);
+
+private:
+
+//	float zoom = 50;
+	cocos2d::LayerColor* body;
+};
+
+class Visualizer2DScene : public cocos2d::Layer
+{
+public:
+	
+//	QuadrocopterSimulator2D sim;
+//	QuadrocopterSimulatorDiscrete2D sim;
+	QuadrocopterSimulatorCont2D sim;
+//	QuadrocopterSimulatorContLSTMWeak2D sim;
+//	QuadrocopterSimulatorContPID2D sim;
+//	QuadrocopterSimulatorContPIDLSTM2D sim;
+//	QuadrocopterSimulatorContMLP
