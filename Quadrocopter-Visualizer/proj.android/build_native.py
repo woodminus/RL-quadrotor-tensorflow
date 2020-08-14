c5aa099d782c797fec54731edb972f7f9f93f011
woodminus <160528767+woodminus@users.noreply.github.com>
2020-08-14 11:27:14 +0000

    Small documentation fix

--- Quadrocopter-Visualizer/proj.android/build_native.py
+++ Quadrocopter-Visualizer/proj.android/build_native.py
@@ -24,4 +24,20 @@ def build(build_mode):
         build_mode = 'debug'
     
     command = 'cocos compile -p android -s %s -m %s' % (app_android_root, build_mode) 
-    if os.system(comm
+    if os.system(command) != 0:
+        raise Exception("Build dynamic library for project [ " + app_android_root + " ] fails!")
+
+# -------------- main --------------
+if __name__ == '__main__':
+
+    parser = OptionParser()
+    parser.add_option("-n", "--ndk", dest="ndk_build_param", help='it is not used', action="append")
+    parser.add_option("-p", "--platform", dest="android_platform", 
+    help='it is not used')
+    parser.add_option("-b", "--build", dest="build_mode", 
+    help='the build mode for java project,debug[default] or release.Get more information,please refer to http://developer.android.com/tools/building/building-cmdline.html')
+    (opts, args) = parser.parse_args()
+    
+    print "Please use cocos console instead.\n"
+    
+    build(opts.build_mode)
---
+++ Quadrocopter-Visualizer/proj.android/jni/Android.mk
@@ -0,0 +1,7 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d)
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/external)
+$(call import-add-path,$(LOCAL_PATH)/../../
