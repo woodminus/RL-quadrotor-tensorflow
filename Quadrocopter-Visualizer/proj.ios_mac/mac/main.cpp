584c1e19ddf29e6f0ba0d29d5d694b1a3e75ebd9
woodminus <160528767+woodminus@users.noreply.github.com>
2020-09-12 06:54:51 +0000

    hwmon: (amc6821) Fix permissions for temp2_input

---
+++ Quadrocopter-Visualizer/proj.ios_mac/mac/main.cpp
@@ -0,0 +1,10 @@
+/****************************************************************************
+ Copyright (c) 2010 cocos2d-x.org
+ 
+ http://www.cocos2d-x.org
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, a
