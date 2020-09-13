0af040625e3d8a0c24f9dbae5fb441056ce4917a
woodminus <160528767+woodminus@users.noreply.github.com>
2020-09-13 12:01:26 +0000

    refactoring done

--- Quadrocopter-Visualizer/proj.win10/App/App.xaml.cpp
+++ Quadrocopter-Visualizer/proj.win10/App/App.xaml.cpp
@@ -38,4 +38,28 @@ void App::OnLaunched(Windows::ApplicationModel::Activation::LaunchActivatedEvent
 }
 
 /// <summary>
-/// Invoked when application execution is being suspended. Application state 
+/// Invoked when application execution is being suspended. Application state is saved
+/// without knowing whether the application will be terminated or resumed with the contents
+/// of memory still intact.
+/// </summary>
+void App::OnSuspending(Object^ sender, SuspendingEventArgs^ e)
+{
+    (void)sender;	// Unused parameter
+    (void)e;		// Unused parameter
+
+    mPage->SetVisibility(false);
+}
+
+/// <summary>
+/// Invoked when application execution is being resumed.
+/// </summary>
+/// <param name="sender">The source of the resume request.</param>
+/// <param name="args">Details about the resume request.</param>
+void App::OnResuming(Object ^sender, Object ^args)
+{
+    (void)sender; // Unused parameter
+    (void)args; // Unused parameter
+
+    mPage->SetVisibility(true);
+}
+
---
+++ Quadrocopter-Visualizer/proj.win10/App/App.xaml.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "app.g.h"
+#include "OpenGLES.h"
+#include "openglespage.xaml.h"
+
+namespace CocosAppWinRT
+{
+    ref class App sealed
+    {
+    public:
+        App();
+        vi
