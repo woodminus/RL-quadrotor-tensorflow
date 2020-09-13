4dfd91e41b60af3197515561910a820926669844
woodminus <160528767+woodminus@users.noreply.github.com>
2020-09-13 11:39:28 +0000

    Update LeetCode.md

--- Quadrocopter-Visualizer/proj.win10/App/App.xaml.cpp
+++ Quadrocopter-Visualizer/proj.win10/App/App.xaml.cpp
@@ -11,4 +11,31 @@ using namespace Windows::UI::Xaml;
 using namespace Windows::UI::Xaml::Controls;
 using namespace Windows::UI::Xaml::Controls::Primitives;
 using namespace Windows::UI::Xaml::Data;
-using namespace
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Interop;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+using namespace cocos2d;
+using namespace CocosAppWinRT;
+
+App::App()
+{
+    InitializeComponent();
+    Suspending += ref new SuspendingEventHandler(this, &App::OnSuspending);
+    Resuming += ref new EventHandler<Object^>(this, &App::OnResuming);
+}
+
+void App::OnLaunched(Windows::ApplicationModel::Activation::LaunchActivatedEventArgs^ e)
+{
+    if (mPage == nullptr)
+    {
+        mPage = ref new OpenGLESPage(&mOpenGLES);
+    }
+
+    // Place the page in the current window and ensure that it is active.
+    Windows::UI::Xaml::Window::Current->Content = mPage;
+    Windows::UI::Xaml::Window::Current->Activate();
+}
+
+/// <summary>
+/// Invoked when application execution is being suspended. Application state 
