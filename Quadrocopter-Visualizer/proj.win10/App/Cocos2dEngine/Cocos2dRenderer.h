630f9d0513f1635102bc1d8209c6b0f679d27968
woodminus <160528767+woodminus@users.noreply.github.com>
2020-09-13 22:35:02 +0000

    Staging Data script.

--- Quadrocopter-Visualizer/proj.win10/App/Cocos2dEngine/Cocos2dRenderer.h
+++ Quadrocopter-Visualizer/proj.win10/App/Cocos2dEngine/Cocos2dRenderer.h
@@ -9,4 +9,28 @@
 * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
 *
-* http://www.apache.org/licenses/LI
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and limitations under the License.
+*/
+#pragma once
+
+#include <agile.h>
+
+#include "cocos2d.h"
+
+
+class AppDelegate;
+
+class Cocos2dRenderer
+{
+public:
+    Cocos2dRenderer(int width, int height, float dpi, 
+        Windows::Graphics::Display::DisplayOrientations orientation, 
+        Windows::UI::Core::CoreDispatcher^ dispathcer, Windows::UI::Xaml::Controls::Panel^ panel);
+    ~Cocos2dRenderer();
+    void Draw(GLsizei width, GLsizei height, float dpi, Windows::Graphics::Display::DisplayOrientations orientation);
+	void QueuePointerEvent(cocos2d::PointerEventType type, Windows::UI::Core::PointerEventArgs^ args);
+	void QueueKeyboardEvent(cocos2d::WinRTKeyboardEventTyp
