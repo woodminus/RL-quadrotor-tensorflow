767e088dc93efc026e2b419b5e256aeb67c8b62e
woodminus <160528767+woodminus@users.noreply.github.com>
2020-11-25 13:38:38 +0000

    update pywps

--- Quadrocopter2DBrain/Quadrocopter2DBrain/ExperienceBank.cpp
+++ Quadrocopter2DBrain/Quadrocopter2DBrain/ExperienceBank.cpp
@@ -26,4 +26,36 @@ void ExperienceBank::store (const ExperienceItem& expItem) {
 //	std::cerr << "--- ExperienceBank " << name << ": " << experienceI << " / " << experience.size() << std::endl;
 }
 
-v
+void ExperienceBank::store (const std::vector<const ExperienceItem*>& minibatch) {
+	for (auto item : minibatch) {
+		store (*item);
+	}
+}
+
+int ExperienceBank::getSize () const {
+	return size;
+}
+
+int ExperienceBank::getCurrentIndex () const {
+	return experienceI;
+}
+
+void ExperienceBank::setName (const std::string& name) {
+	this->name = name;
+}
+
+const ExperienceItem& ExperienceBank::getRandomExp () const {
+	return experience [Lib::randInt(0, size-1)];
+}
+
+void getMinibatch (
+	const ExperienceBank& bank,
+	std::vector<const ExperienceItem*>& minibatch,
+	int minibatchSize
+) {
+	minibatch.clear();
+
+	for (int i=0; i<minibatchSize; i++) {
+		minibatch.push_back (&bank.getRandomExp());
+	}
+}
---
+++ Quadrocopter2DBrain/Quadrocopter2DBrain/ExperienceBank.hpp
@@ -0,0 +1,12 @@
+//
+//  ExperienceBank.hpp
+//  Quadrocopter2DBrain
+//
+//  Created by anton on 17/04/16.
+//  Copyright © 2016 anton. All rights reserved.
+//
+
+#ifndef ExperienceBank_hpp
+#define ExperienceBank_hpp
+
+#include "Experie
