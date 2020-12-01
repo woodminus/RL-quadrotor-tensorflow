e15f5145805e36f83824f95d3f57ede1a1b0d372
woodminus <160528767+woodminus@users.noreply.github.com>
2020-12-01 08:43:30 +0000

    Release 0.15.1

--- Quadrocopter2DBrain/Quadrocopter2DBrain/ExperienceBank.hpp
+++ Quadrocopter2DBrain/Quadrocopter2DBrain/ExperienceBank.hpp
@@ -9,4 +9,38 @@
 #ifndef ExperienceBank_hpp
 #define ExperienceBank_hpp
 
-#include "Experie
+#include "ExperienceItem.hpp"
+
+class ExperienceBank {
+public:
+	
+	ExperienceBank ();
+	
+	void store (const ExperienceItem& expItem);
+	void store (const std::vector<const ExperienceItem*>& minibatch);
+	
+	void setName (const std::string& name);
+	
+	int getSize () const;
+	int getCurrentIndex () const;
+	const ExperienceItem& getRandomExp () const;
+	
+private:
+
+	std::string name;
+	
+	std::vector<ExperienceItem> experience;
+	
+	int size = 0;
+	int experienceI = 0;
+	static const int maxExperience = 3000000;
+
+};
+
+void getMinibatch (
+	const ExperienceBank& bank,
+	std::vector<const ExperienceItem*>& minibatch,
+	int minibatchSize
+);
+
+#endif /* ExperienceBank_hpp */
---
+++ Quadrocopter2DBrain/Quadrocopter2DBrain/ExperienceFilter.hpp
@@ -0,0 +1,13 @@
+
+//
+//  ExperienceFilter.hpp
+//  Quadrocopter2DBrain
+//
+//  Created by anton on 26/03/16.
+//  Copyright © 2016 anton. All rights reserved.
+//
+
+#ifndef ExperienceFilter_hpp
+#define ExperienceFilter_hpp
+
+#include "QuadrocopterBrain.hpp"
