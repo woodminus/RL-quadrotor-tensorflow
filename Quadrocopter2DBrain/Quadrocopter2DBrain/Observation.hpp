4a969181f54ed415dc19788c748d5cb984c1e8c1
woodminus <160528767+woodminus@users.noreply.github.com>
2020-12-13 13:59:47 +0000

    Transferred from the Adobe Open Source Subversion Repository

---
+++ Quadrocopter2DBrain/Quadrocopter2DBrain/Observation.hpp
@@ -0,0 +1,16 @@
+//
+//  Observation.hpp
+//  QuadrocopterBrain
+//
+//  Created by anton on 21/01/16.
+//  Copyright © 2016 anton. All rights reserved.
+//
+
+#ifndef Observation_hpp
+#define Observation_hpp
+
+#include <vector>
+
+class Observation {
+
+pub
