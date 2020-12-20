57d1d03629e35a61c89c19ebcfad58a8152320cc
woodminus <160528767+woodminus@users.noreply.github.com>
2020-12-20 05:32:23 +0000

    Minor fix for location vstats

--- Quadrocopter2DBrain/Quadrocopter2DBrain/Quadrocopter2DBrain.cpp
+++ Quadrocopter2DBrain/Quadrocopter2DBrain/Quadrocopter2DBrain.cpp
@@ -28,4 +28,32 @@ namespace Quadrocopter2DBrain {
 
   QuadrocopterBrain quadrocopterBrain (std::shared_ptr<BrainAlgorithm> (new DDPG ()));
 //	QuadrocopterBrain quadrocopterBrain (std::shared_ptr<BrainAlgorithm> (new DDPG_LSTM ()));
-//	QuadrocopterBrain quadrocopt
+//	QuadrocopterBrain quadrocopterBrain (std::shared_ptr<BrainAlgorithm> (new DDPG_LSTM_Weak ()));
+	
+	const bool useLstmWeak = false;
+//	const bool useLstmWeak = true;
+	
+	//lstm
+	vector<ObservationSeqLimited> prevObsSeq;
+	vector<ObservationSeqLimited> nextObsSeq;
+	vector<ObservationSeqLimited> lstmActions;
+	vector<ObservationSeqLimited> lstmRewards;
+	
+	vector<ExpLambdaFilter> experienceFilters; //one filter for each quadrocopter;
+	vector<double> randomnessOfQuadrocopter;
+	
+	vector<ExperienceItem> lstmWeakExperience; // by copter
+	vector<ExperienceItem*> lstmWeakExperienceMinibatch;
+
+	vector<ObservationSeqLimited> prevObsMLPSeq;
+	vector<ObservationSeqLimited> nextObsMLPSeq;
+
+
+//	void setQuadrocopterState(
+//		int quadrocopterId,
+//		const std::vector<float>& stateV
+//	) {
+//
+//	//	std::vector<float> stateV;
+//	//	stateV.resize(QuadrocopterBrain::observationSize);
+//	//	s
