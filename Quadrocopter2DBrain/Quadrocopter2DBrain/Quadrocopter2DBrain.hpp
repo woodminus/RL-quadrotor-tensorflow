268955a9f4199c3561868f1d3f86cc11df78e308
woodminus <160528767+woodminus@users.noreply.github.com>
2021-01-04 06:28:40 +0000

    drm/i915: do not default to 18 bpp for eDP if missing from VBT

--- Quadrocopter2DBrain/Quadrocopter2DBrain/Quadrocopter2DBrain.hpp
+++ Quadrocopter2DBrain/Quadrocopter2DBrain/Quadrocopter2DBrain.hpp
@@ -14,4 +14,48 @@
 
 namespace Quadrocopter2DBrain {
 
-	void initApiDiscreteDeepQ ()
+	void initApiDiscreteDeepQ ();
+
+//	void setQuadrocopterState(
+//		int quadrocopterId,
+//		const std::vector<float>& stateV
+//	);
+
+	long quadrocopterBrainAct(
+		int quadrocopterId,
+		const std::vector<float>& state
+	);
+	
+	void quadrocopterBrainActCont(
+		int quadrocopterId,
+		const std::vector<float>& state,
+		std::vector<float>& action
+	);
+	
+	void quadrocopterBrainActContLSTM(
+		int quadrocopterId,
+		const std::vector<float>& state,
+		std::vector<float>& action
+	);
+	
+	void quadrocopterBrainActContLSTMWeak(
+		int quadrocopterId,
+		const std::vector<float>& state,
+		std::vector<float>& action
+	);
+	
+	void quadrocopterBrainActContMLPSeq(
+		int quadrocopterId,
+		const std::vector<float>& state,
+		std::vector<float>& action
+	);
+	
+	bool quadrocopterBrainTrain ();
+
+	void storeQuadrocopterExperience (
+		int quadrocopterId,
+		double reward,
+		long action,
+		const std::vector <float>& prevState,
+		const std::vector <float>& nextState
+	);
