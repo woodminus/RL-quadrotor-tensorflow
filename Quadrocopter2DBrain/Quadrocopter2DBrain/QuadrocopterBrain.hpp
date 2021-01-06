8bae937406f1f6fc0b9d87f02eb99c740fe17aab
woodminus <160528767+woodminus@users.noreply.github.com>
2021-01-06 09:31:04 +0000

    cpufreq: interactive: set floor for boosted speed

--- Quadrocopter2DBrain/Quadrocopter2DBrain/QuadrocopterBrain.hpp
+++ Quadrocopter2DBrain/Quadrocopter2DBrain/QuadrocopterBrain.hpp
@@ -19,4 +19,33 @@
 
 class QuadrocopterBrain {
 
-publi
+public:
+	
+	static const int observationsInSeq = 1;
+
+	//2d
+	static const int observationSize = 50;
+	static const int lstmStepsCount = 6;
+	static const int lstmStateSize = 128;
+	static const int numActions = 9;
+	static const int contActionSize = 2;
+	static const int mlpSeqSize = 4;
+	
+//	static const bool useObsSeq = true;
+	static const bool useObsSeq = false;
+
+	static const int minibatchSize = 128;
+
+	//1d
+//	static const int observationSize = 4;
+//	static const int numActions = 3;
+//	static const int contActionSize = 1;
+
+	QuadrocopterBrain ();
+	QuadrocopterBrain (std::shared_ptr<BrainAlgorithm> algorithm);
+	
+	long act (const ObservationSeqLimited& state, double randomness);
+	void actCont (const ObservationSeqLimited& state, std::vector<float>& action, double randomness);
+	void actContLSTMWeak (
+		const ObservationSeqLimited& state,
+		const std::vector<fl
