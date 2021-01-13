b493f30ce95a497eed44b1ca304f48ad9f988df5
woodminus <160528767+woodminus@users.noreply.github.com>
2021-01-13 15:41:58 +0000

    Bump to version 0.1.1

--- TensorflowGraph/create_graph_2d_ddpg.py
+++ TensorflowGraph/create_graph_2d_ddpg.py
@@ -24,4 +24,24 @@ num_actions = 2;
 #critic = MLP([input_size, num_actions*2], [2048, 1024, 1],
 #            [tf.nn.sigmoid, tf.nn.sigmoid, tf.identity], scope='critic')
 #
-#actor = MLP([input_size,], [2048, 1024, num_act
+#actor = MLP([input_size,], [2048, 1024, num_actions],
+#            [tf.nn.sigmoid, tf.nn.sigmoid, tf.identity], scope='actor')
+
+r = tf.nn.relu
+t = tf.nn.tanh
+
+critic = MLP([input_size, num_actions], [2048, 512, 256, 256, 1],
+            [t, t, t, t, tf.identity], scope='critic')
+
+actor = MLP([input_size,], [2048, 512, 256, 256, num_actions],
+            [t, t, t, t, tf.identity], scope='actor')
+
+# The optimizer to use. Here we use RMSProp as recommended
+# by the publication
+#optimizer = tf.train.RMSPropOptimizer(learning_rate= 0.0001, decay=0.9)
+#optimizer = tf.train.RMSPropOptimizer(learning_rate= 0.0005, decay=0.9)
+optimizer = tf.train.AdamOptimizer(learning_rate= 0.0001)
+#optimizer = tf.train.GradientDescentOptimizer(learning_rate= 0.001)
+
+# DiscreteDeepQ object
+current_controller = ContinuousDeepQ(input_size, num_actions, actor, critic, optimizer, session, discount_rate=0.99, target_actor_u
