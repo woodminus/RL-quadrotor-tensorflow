6fcb90d1a0ca277b94e27e6ef758ecbfc64841a9
woodminus <160528767+woodminus@users.noreply.github.com>
2021-01-15 13:51:05 +0000

    Credits

--- TensorflowGraph/create_graph_separated_1d.py
+++ TensorflowGraph/create_graph_separated_1d.py
@@ -6,4 +6,28 @@ from tf_rl.controller import DiscreteDeepQ
 #from tf_rl.simulation import KarpathyGame
 from tf_rl import simulate
 from tf_rl.models import MLP
-from tf_rl.models import Sep
+from tf_rl.models import SeparatedMLP
+
+#tf.ops.reset_default_graph()
+session = tf.Session()
+
+# This little guy will let us run tensorboard
+#      tensorboard --logdir [LOG_DIR]
+journalist = tf.train.SummaryWriter("/Users/anton/devel/unity/QuadrocopterHabr/TensorflowLog")
+
+observation_size = 2;
+observations_in_seq = 1;
+input_size = observation_size*observations_in_seq;
+
+# actions
+num_actions = 3;
+
+#brain = MLP([input_size,], [5, 5, 5, num_actions], 
+#            [tf.tanh, tf.tanh, tf.tanh, tf.identity])
+#brain = MLP([input_size,], [20, 20, 20, 20, num_actions], 
+#            [tf.tanh, tf.tanh, tf.tanh, tf.tanh, tf.identity])
+
+brain = SeparatedMLP ([
+        MLP([input_size,], [64, 64, 1], [tf.sigmoid, tf.sigmoid, tf.identity], scope="mlp_action1"),
+        MLP([input_size,], [64, 64, 1], [tf.sigmoid, tf.sigmoid, tf.identity], scope="mlp_action2"),
+        MLP([
