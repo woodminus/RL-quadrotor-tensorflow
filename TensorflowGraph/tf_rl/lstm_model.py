d4272e517b55ace66aea3b316834f0edb6541d80
woodminus <160528767+woodminus@users.noreply.github.com>
2021-06-09 10:01:18 +0000

    Update emission query map

--- TensorflowGraph/tf_rl/lstm_model.py
+++ TensorflowGraph/tf_rl/lstm_model.py
@@ -14,4 +14,22 @@ class LSTMModel(object):
             self.cell = tf.nn.rnn_cell.LSTMCell(self.layer_size, state_is_tuple=True)
 
             self.states  = tf.nn.rnn_cell.LSTMStateTuple (tf.placeholder(tf.float32, (None, self.layer_size), name="state_c"), tf.placeholder(tf.float32, (None, self.layer_size), name="state_h"))
-            print 
+            print "placeholder state c: " + str(self.states [0])
+            print "placeholder state h: " + str(self.states [1])
+
+            xs  = tf.placeholder(tf.float32, (None, input_size), name="fake_xs")
+            val, states = self.cell (xs, self.states)
+#            if (needReuseVariables):
+#                vs.reuse_variables()
+            print "LSTMModel vs: ->" + vs.name + "<- reuse: " + str(vs.reuse)
+            
+            self.model_variables = [v for v in tf.trainable_variables()
+                    if v.name.startswith(vs.name)]
+            for v in self.model_variables:
+                print "LSTMModel v: " + v.name
+
+    def __call__(self, xs, states=None):
+        with tf.variable_scope(self.scope, reuse=True) as vs:
+            input_states = states or self.states
+            val, states = self.cell (xs, self.states)
+            self.out_state_c = tf.identity (sta
