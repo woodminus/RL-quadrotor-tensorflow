40414af1655e7fcee3f089e9ef8d87ae5cf34705
woodminus <160528767+woodminus@users.noreply.github.com>
2021-07-21 09:12:23 +0000

    fix paging

--- TensorflowGraph/tf_rl/lstm_multi_model.py
+++ TensorflowGraph/tf_rl/lstm_multi_model.py
@@ -19,4 +19,17 @@ class LSTMMultiModel(object):
 
             self.cell = tf.nn.rnn_cell.LSTMCell(self.layer_size, state_is_tuple=True)
             self.cell2 = tf.nn.rnn_cell.LSTMCell(self.layer_size, state_is_tuple=True)
-#      
+#            self.multi_cell = tf.nn.rnn_cell.MultiRNNCell([cell] * self.layers_count, state_is_tuple=True)
+            
+            self.states  = tf.nn.rnn_cell.LSTMStateTuple (tf.placeholder(tf.float32, (None, self.layer_size), name="state_c"), tf.placeholder(tf.float32, (None, self.layer_size), name="state_h"))
+            xs  = tf.placeholder(tf.float32, (None, input_size), name="xs")
+            xs_prev  = tf.placeholder(tf.float32, (None, input_size), name="xs_prev")
+            
+            val, state = self.cell  (xs, self.states)
+            val, state = self.cell2 (xs_prev, state)
+
+            w = tf.truncated_normal([self.layer_size, self.output_size]);
+            self.weight = tf.get_variable ("weight", w.get_shape(), initializer=lambda x,dtype=tf.float32: w)
+
+            b = tf.constant(0.1, shape=[self.output_size])
+            self.bias = tf.get_variable ("bias", b.get_shape(), initializer=lambda
