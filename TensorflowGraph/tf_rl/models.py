2331775c914e5d44179d78d24027df62d598645b
woodminus <160528767+woodminus@users.noreply.github.com>
2021-09-10 19:29:14 +0000

    Schedule stale claimed environments for destruction

--- TensorflowGraph/tf_rl/models.py
+++ TensorflowGraph/tf_rl/models.py
@@ -22,4 +22,26 @@ class Layer(object):
                 W_initializer =  tf.random_uniform_initializer(
                         -0.003, 0.003)
                 W_var = tf.get_variable(W_name, (input_size, output_size), initializer=W_initializer)
-                g
+                get_W_index += 1
+                self.Ws.append(W_var)
+            self.b = tf.get_variable("b", (output_size,), initializer=tf.constant_initializer(0))
+
+    def __call__(self, xs):
+        if type(xs) != list:
+            xs = [xs]
+        assert len(xs) == len(self.Ws), \
+                "Expected %d input vectors, got %d" % (len(self.Ws), len(xs))
+        with tf.variable_scope(self.scope):
+            return sum([tf.matmul(x, W) for x, W in zip(xs, self.Ws)]) + self.b
+
+    def variables(self):
+        return [self.b] + self.Ws
+
+    def copy(self, scope=None):
+        scope = scope or self.scope + "_copy"
+
+        with tf.variable_scope(scope) as sc:
+            for v in self.variables():
+                tf.get_variable(base_name(v), v.get_shape(), initializer=lambda x,dtype=tf.float32, partition_info=None: v.initialized_value())
+            sc.reuse_variables()
+            return Layer(self.input_sizes, self.output_size
