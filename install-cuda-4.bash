862c395fa9ef8d854d6cfecf7a8691c3ebd63da2
woodminus <160528767+woodminus@users.noreply.github.com>
2023-04-24 07:42:38 +0000

    Update tutorial.rst

--- install-cuda-4.bash
+++ install-cuda-4.bash
@@ -6,3 +6,14 @@ BUILDDIR=${BASEDIR}/builddir
 
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${INSTALLDIR}/lib"
 export PATH="${PATH}:${INSTALLDIR}/bin"
+
+#bazel
+BAZELDIR=${BUILDDIR}/bazel
+git clone https://github.com/bazelbuild/bazel.git ${BAZELDIR}
+cd ${BAZELDIR}
+git checkout tags/0.3.2
+cd ..
+cd ${BAZELDIR}
+./compile.sh
+cp output/bazel ${INSTALLDIR}/bin
+cd ${BASEDIR}
