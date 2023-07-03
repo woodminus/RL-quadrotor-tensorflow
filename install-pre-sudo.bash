e0dde9e2d4eff73dcd0703164ee40b727d99ec2b
woodminus <160528767+woodminus@users.noreply.github.com>
2023-07-03 14:56:58 +0000

    build: update dependencies

--- install-pre-sudo.bash
+++ install-pre-sudo.bash
@@ -13,4 +13,27 @@ sudo apt-get install -y build-essential python-pip python-dev git python-numpy s
 ## Blacklist Noveau which has some kind of conflict with the nvidia driver
 ##echo -e "blacklist nouveau\nblacklist lbm-nouveau\noptions nouveau modeset=0\nalias nouveau off\nalias lbm-nouveau off\n" | sudo tee /etc/modprobe.d/blacklist-nouveau.conf
 ##echo options nouveau modeset=0 | sudo tee -a /etc/modprobe.d/nouveau-kms.conf
-##sudo update
+##sudo update-initramfs -u
+##sudo reboot # Reboot (annoying you have to do this in 2015!)
+
+sudo apt-get install -y linux-image-extra-virtual
+
+# Install latest Linux headers
+sudo apt-get install -y linux-source linux-headers-`uname -r` 
+
+sudo apt-get install libpng-dev libjpeg-dev libxxf86vm1 libxxf86vm-dev libxi-dev libxrandr-dev mesa-common-dev mesa-utils-extra libgl1-mesa-dev libglapi-mesa libglfw-dev -y
+
+
+##bazel
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer -y
+##sudo apt-get install software-properties-common -y
+##echo "deb http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+##curl https://storage.googleapis.com/bazel-apt/doc/apt-key.pub.gpg | sudo apt-key add -
+##sudo apt-get update && sudo apt-get install bazel -y
+
+sudo update-alternatives --config java
+sudo update-alternatives --config javac
+
+sudo apt-get inst
